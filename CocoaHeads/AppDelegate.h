4d01b5cd8e70ef0748dac04690c96e46188444b3
Rodrigo Andrade <rodrigo.andrade@me.com>
2015-03-24 20:08:47 -0300

    Initial Commit

---
+++ CocoaHeads/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoaHeads
+//
+//  Created by Rodrigo Andrade on 3/24/15.
+//  Copyright (c) 2015 Rodrigo Andrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocoaHeads/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoaHeads
+//
+//  Created by Rodrigo Andrade on 3/24/15.
+//  Copyright (c) 2015 Rodrigo Andrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
