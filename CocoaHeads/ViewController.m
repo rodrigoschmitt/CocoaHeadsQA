83c170f9756b87c0744af25c88c1fcf0b3d486de
Rodrigo Andrade <rodrigo.andrade@me.com>
2015-03-26 13:44:52 -0300

    Teste simples.

--- CocoaHeads/ViewController.m
+++ CocoaHeads/ViewController.m
@@ -40,7 +40,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *cocoaHeads = @"Oi mundo!";
+    
 }
 
 - (void)didReceiveMemoryWarning {
